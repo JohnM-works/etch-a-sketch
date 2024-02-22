120e7330476c841482f93547314295f15b129cb9
jhin47 <johnmalonzo63@gmail.com>
2024-02-23 03:39:05 +0800

    adding function to the random and black color button

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const sketchArea = document.querySelector(".sketch-area");
 const gridSize = document.querySelector(".button");
 const slideInput = document.querySelector(".slideInput");
 const slideLabel = document.querySelector(".slide-label");
+const randomBtn = document.querySelector(".random-btn");
+const blackBtn = document.querySelector(".black-btn");
 
 function createGrid(squares) {
   sketchArea.textContent = "";
@@ -14,17 +16,41 @@ function createGrid(squares) {
     gridPixels.style.width = `${100 / squares}%`;
     sketchArea.appendChild(gridPixels);
 
-    //Events
     gridPixels.addEventListener("mouseover", () => {
       gridPixels.style.backgroundColor = "black";
       gridPixels.style.borderStyle = "none";
     });
+
+    //black color event
+    blackBtn.addEventListener("click", () => {
+      gridPixels.addEventListener("mouseover", blackColor);
+    });
+
+    //random color event
+    randomBtn.addEventListener("click", () => {
+      gridPixels.addEventListener("mouseover", randColors);
+    });
   }
+}
+
+function blackColor(color) {
+  color.target.style.backgroundColor = "black";
+  color.target.style.borderStyle = "none";
+}
+
+function randColors(event) {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  color = `rgb(${red}, ${green}, ${blue})`;
+
+  event.target.style.backgroundColor = color;
 }
 
 slideInput.addEventListener("input", (slideEvent) => {
   inputSize = slideEvent.target.value;
-  slideLabel.textContent = `${inputSize} X ${inputSize}`;
+  slideLabel.textContent = `Choose Size: ${inputSize}x${inputSize}`;
   createGrid(inputSize);
 });
 
