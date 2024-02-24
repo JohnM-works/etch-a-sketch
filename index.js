cc0498d45cc03f8ae593cc9c8cd56263d11990f8
jhin47 <johnmalonzo63@gmail.com>
2024-02-25 04:12:16 +0800

    adding function to the grid button and color picker

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const slideInput = document.querySelector(".slideInput");
 const slideLabel = document.querySelector(".slide-label");
 const randomBtn = document.querySelector(".random-btn");
 const blackBtn = document.querySelector(".black-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const gridBtn = document.querySelector("#grid-btn");
+const colorPicker = document.querySelector("#colorPicker");
 
 function createGrid(squares) {
-  sketchArea.textContent = "";
-
   for (let i = 0; i < squares * squares; i++) {
     let gridPixels = document.createElement("div");
     gridPixels.classList.add("cell");
@@ -15,27 +16,11 @@ function createGrid(squares) {
     gridPixels.style.height = `${100 / squares}%`;
     gridPixels.style.width = `${100 / squares}%`;
     sketchArea.appendChild(gridPixels);
-
-    gridPixels.addEventListener("mouseover", () => {
-      gridPixels.style.backgroundColor = "black";
-      gridPixels.style.borderStyle = "none";
-    });
-
-    //black color event
-    blackBtn.addEventListener("click", () => {
-      gridPixels.addEventListener("mouseover", blackColor);
-    });
-
-    //random color event
-    randomBtn.addEventListener("click", () => {
-      gridPixels.addEventListener("mouseover", randColors);
-    });
   }
 }
 
 function blackColor(color) {
   color.target.style.backgroundColor = "black";
-  color.target.style.borderStyle = "none";
 }
 
 function randColors(event) {
@@ -44,12 +29,77 @@ function randColors(event) {
   const blue = Math.floor(Math.random() * 255);
 
   color = `rgb(${red}, ${green}, ${blue})`;
-
   event.target.style.backgroundColor = color;
 }
 
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function toggleOutline() {
+  const squares = document.querySelectorAll(".cell");
+  squares.forEach((square) => {
+    square.classList.toggle("square");
+  });
+}
+
+function pickColor() {}
+
+//-------EVENTS------//
+
+//random color event
+randomBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".cell");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", randColors);
+  });
+});
+
+//black color event
+blackBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".cell");
+  squares.forEach((square) => {
+    square.removeEventListener("mouseover", randColors);
+    square.addEventListener("mouseover", blackColor);
+  });
+});
+
+//grid button event
+gridBtn.addEventListener("click", () => {
+  toggleOutline();
+  gridBtn.classList.toggle("active");
+  if (gridBtn.value == "ON") {
+    gridBtn.value == "OFF";
+  } else {
+    gridBtn.value == "ON";
+  }
+});
+
+//clear button event
+clearBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".cell");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
+
+//Color Picker event
+colorPicker.addEventListener("change", (event) => {
+  let color = event.target.value;
+  const squares = document.querySelectorAll(".cell");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = color;
+    });
+  });
+});
+
+//slider event
 slideInput.addEventListener("input", (slideEvent) => {
   inputSize = slideEvent.target.value;
+  removeAllChildNodes(sketchArea);
   slideLabel.textContent = `Choose Size: ${inputSize}x${inputSize}`;
   createGrid(inputSize);
 });
